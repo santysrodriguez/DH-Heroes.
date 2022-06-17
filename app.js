b9bba57f6dc72647e7549d141b9891912a9414a8
santysrodriguez <santiagocarp987@gmail.com>
2022-06-16 23:19:45 -0300

    agrego estructura

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const port = 3030;
+
+app.listen(port, () => console.log('Server running in http://localhost:' +  port))
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP Heroes</title>
+</head>
+<body>
+    <h1>Heroes</h1>
+</body>
+</html>
